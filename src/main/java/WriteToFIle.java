af9d65b25d0a023b4df9915eedb253c603ebed76
GA <jack.makrides@btinternet.com>
2019-06-22 17:16:30 +0100

    First attempts at read/write

--- src/main/java/WriteToFIle.java
+++ src/main/java/WriteToFIle.java
@@ -1,11 +1,25 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 public class WriteToFIle {
+
+    public static void createBatchNumberFile() throws IOException {
+        File batchnumbers = new File("files/batchnumbers/batchnumbers.txt");
+
+        batchnumbers.createNewFile();
+
+
+    }
 
     public static void writeBatchNumberToFile(String batchNo) throws IOException {
         JSONObject batch = new JSONObject();
@@ -13,6 +27,14 @@ public class WriteToFIle {
 
         try (FileWriter file = new FileWriter("files/batchnumbers/batchnumbers.txt")) {
             file.write(batch.toJSONString());
+        }
+
+        try {
+            final Path path = Paths.get("files/batchnumbers/batchnumbers.txt");
+            Files.write(path, batch.toJSONString().getBytes(), StandardCharsets.UTF_8,
+                    Files.exists(path) ? StandardOpenOption.APPEND : StandardOpenOption.CREATE);
+        } catch (final IOException ioe) {
+            // Add your own exception handling...
         }
     }
 }
